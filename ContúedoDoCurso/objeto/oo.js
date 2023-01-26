740dc95d06b235d44f8c27870630e16b3d0c5304
Lucas <lucasdoismile16@gmail.com>
2023-01-26 19:59:39 -0300

    terminei a pasta de funcao e iniciei uma pasta de exercicios

---
+++ ContúedoDoCurso/objeto/oo.js
@@ -0,0 +1,32 @@
+// CÓDIGO NÃO EXECUTAVEL
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+//00
+obbjeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //  ...
+    }
+}
+objeto.processamento()
+
+/*
+Principios importantes do OO:
+1 - Abstração -> Filtrar as características e comportamentos de um objeto
+2 - Encapsulamento -> Ideia de esconder detalhes da aplicação, como variáveis, métodos
+privados...
+3 - Herança(prototype) -> O ato de reaproveitar de códigos de uma classe em comum, ideia de pai e ilho, superclasse e subclasse. Sendo possível a subclasse herdar tributos e métodos do pai e ainda por cima criar novos!
+4 - Polimorfismo -> É o ato de instaciar várias vezes a mesma classe criando assim um objeto com comportamentos diferentes
+ex:Class Animal{} e a Class Dog extends Animal{}
+*/
+
+
+
+/*Conceitos importantes:
+Classes concretas podem ser instanciadas
+Classes abstratas não! Obs: JavaScript não possui classes abstratas 
+*/
